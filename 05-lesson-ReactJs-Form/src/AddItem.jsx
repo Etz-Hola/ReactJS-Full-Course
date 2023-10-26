f4f6282064218cd1ed934b488d6962e520f1ca2b
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-10-26 16:01:41 +0100

    done with react form

--- 05-lesson-ReactJs-Form/src/AddItem.jsx
+++ 05-lesson-ReactJs-Form/src/AddItem.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { FaPlus} from 'react-icons/fa'
+import {useRef} from 'react'
 
 
 const  AddItem = ({newItem, setNewItem, handleSubmit}) => {
+    const inputRef = useRef()
+
   return (
     <form className='addForm' onSubmit={handleSubmit}>
         <label htmlFor="addItem">Add Item</label>
         <input id="addItem"
+            autoFocus
+            ref={inputRef}
             type='text'
             placeholder='add item'
             required
