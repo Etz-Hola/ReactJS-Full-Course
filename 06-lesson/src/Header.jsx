78656d4c5e1716e8d886e9ffd3555a186f5e045a
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-10-30 10:29:02 +0100

    update

---
+++ 06-lesson/src/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './index.css'
+
+const Header = (props) => {
+  
+  
+  return (
+    <header>
+      <h1>{props.title}</h1>
+      
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  title: "Default Title"
+}
+
+export default Header
