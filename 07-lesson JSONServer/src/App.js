7772256e3a5ff188368fdd950adc2d77143a94b4
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-10-30 10:50:53 +0100

    update

---
+++ 07-lesson JSONServer/src/App.js
@@ -0,0 +1,75 @@
+import { useState, useEffect } from "react";
+import "./index.css";
+import Content from "./Content";
+import Footer from "./Footer";
+import Header from "./Header";
+import AddItem from "./AddItem";
+import SearchItem from "./SearchItem";
+
+function App() {
+  const [newItem, setNewItem] = useState("");
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("Shoppinglist")) || []);
+  const [search, setSearch] = useState("");
+
+  //useEffect syntax
+
+  useEffect(() => {
+    localStorage.setItem("Shoppinglist", JSON.stringify(items));
+  }, [items])
+
+  
+
+  // const setAndSaveItems = (newItem) => {
+  //   setItems(newItem);
+    
+  // };
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = { id, checked: false, item };
+    const listItems = [...items, myNewItem];
+    setItems(listItems);
+  };
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(listItems);
+  };
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    addItem(newItem);
+    setNewItem("");
+  };
+
+  return (
+    <div className="App">
+      <Header title="Cohort 3 list" />
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+      <SearchItem search={search} setSearch={setSearch} />
+
+      <Content
+        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      />
+
+      <Footer length={items.length} />
+    </div>
+  );
+}
+
+export default App;
