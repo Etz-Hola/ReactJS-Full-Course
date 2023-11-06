5a01f12e6e520a21c78441c070d681b044e7f828
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-06 15:57:20 +0100

    update

--- lesson-13 (Data-Context)/src/context/DataContext.jsx
+++ lesson-13 (Data-Context)/src/context/DataContext.jsx
@@ -1,9 +1,8 @@
 import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { format } from "date-fns";
 import useWindowSize from "../hooks/useWindowSize";
 import useAxiosFetch from "../hooks/useAxiosFetch";
-import api from "../api/posts";
+
 
 const DataContext = createContext({})
 
@@ -13,8 +12,6 @@ export const DataProvider = ({ children }) => {
 
     const [search, setSearch] = useState("");
     const [searchResult, setSearchResult] = useState([]);
-    const [editTitle, setEditTitle] = useState("");
-    const [editBody, setEditBody] = useState("");
     const { width } = useWindowSize();
     const { data, fetchError, isLoading } = useAxiosFetch("http://localhost:3500/posts");
    
@@ -31,13 +28,10 @@ export const DataProvider = ({ children }) => {
       setSearchResult(filterResult.reverse());
     }, [posts, search]);
 
-    const navigate = useNavigate();
-
     return (
         <DataContext.Provider value={{
             width, search, setSearch,    //header and nav
             posts, fetchError, isLoading, searchResult, setPosts,  //Home
-            setEditBody, editBody, editTitle, setEditTitle, handleEdit //EditPost
         }}>
             {children}
         </DataContext.Provider>
