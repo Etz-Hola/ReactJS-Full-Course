f447e9565e1072442f2f1b9cafad06bffbbfd0f1
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-06 15:35:02 +0100

    update

--- lesson-13 (Data-Context)/src/context/DataContext.jsx
+++ lesson-13 (Data-Context)/src/context/DataContext.jsx
@@ -8,19 +8,16 @@ import api from "../api/posts";
 const DataContext = createContext({})
 
 export const DataProvider = ({ children }) => {
+  
     const [posts, setPosts] = useState([]);
 
     const [search, setSearch] = useState("");
     const [searchResult, setSearchResult] = useState([]);
-    const [postTitle, setPostTitle] = useState("");
-    const [postBody, setPostBody] = useState("");
     const [editTitle, setEditTitle] = useState("");
     const [editBody, setEditBody] = useState("");
     const { width } = useWindowSize();
-    const { data, fetchError, isLoading } = useAxiosFetch(
-      "http://localhost:3500/posts"
-    );
-  
+    const { data, fetchError, isLoading } = useAxiosFetch("http://localhost:3500/posts");
+   
     useEffect(() => {
       setPosts(data);
     }, [data]);
@@ -35,34 +32,6 @@ export const DataProvider = ({ children }) => {
     }, [posts, search]);
 
     const navigate = useNavigate();
-
-    const handleDelete = async (id) => {
-      try {
-        await api.delete(`/posts/${id}`);
-        const postLists = posts.filter((post) => post.id !== id);
-        setPosts(postLists);
-        navigate("/");
-      } catch (error) {
-        console.log(`Error: ${error.message}`);
-      }
-    };
-  
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-      const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
-      const date = format(new Date(), "MMMM dd, yyyy pp");
-      const newPost = { id, title: postTitle, date, body: postBody };
-      try {
-        const response = await api.post("/posts", newPost);
-        const allPost = [...posts, response.data];
-        setPosts(allPost);
-        setPostTitle("");
-        setPostBody("");
-        navigate("/");
-      } catch (error) {
-        console.log(`Error: ${error.message}`);
-      }
-    };
   
     const handleEdit = async (id) => {
       const date = format(new Date(), "MMMM dd, yyyy pp");
@@ -83,9 +52,7 @@ export const DataProvider = ({ children }) => {
     return (
         <DataContext.Provider value={{
             width, search, setSearch,    //header and nav
-            posts, fetchError, isLoading, searchResult,  //Home
-            postTitle, setPostTitle, postBody, setPostBody, handleSubmit, //NewPost
-            handleDelete, //PostPage
+            posts, fetchError, isLoading, searchResult, setPosts,  //Home
             setEditBody, editBody, editTitle, setEditTitle, handleEdit //EditPost
         }}>
             {children}
