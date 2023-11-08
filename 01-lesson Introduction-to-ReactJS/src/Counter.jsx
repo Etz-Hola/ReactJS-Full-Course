f327dc9b98e543b22c4901e2b3c2d9d5a657ba01
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-08 12:20:40 +0100

    up

--- 01-lesson Introduction-to-ReactJS/src/Counter.jsx
+++ 01-lesson Introduction-to-ReactJS/src/Counter.jsx
@@ -1,22 +1,22 @@
-import { useState} from 'react'
+import { useState } from 'react'
 
 const Counter = () => {
-    const [count, setCount] =useState(0)
+  const [count, setCount] = useState(0)
 
-    const increment = () => {
-        setCount(count + 1)
-    };
+  const increment = () => {
+    setCount(count + 1)
+  };
 
-    const decrement = () => {
-        setCount(count - 1)
-    };
+  const decrement = () => {
+    setCount(count - 1)
+  };
 
 
   return (
     <main>
-        <p>Count: {count}</p>
-        <button onClick={increment}>Increment</button>
-        <button onClick={decrement}>Decrement</button>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </main>
   );
 };
