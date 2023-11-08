405c5e44c2a17c37d95258344fbb0e6e83b3cccb
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-08 12:27:39 +0100

    u

--- 07-lesson Fetch-API-Data/src/AddItem.jsx
+++ 07-lesson Fetch-API-Data/src/AddItem.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
-import { FaPlus} from 'react-icons/fa'
-import {useRef} from 'react'
+import { FaPlus } from 'react-icons/fa'
+import { useRef } from 'react'
 
 
-const  AddItem = ({newItem, setNewItem, handleSubmit}) => {
-    const inputRef = useRef()
+const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
+  const inputRef = useRef()
 
   return (
     <form className='addForm' onSubmit={handleSubmit}>
-        <label htmlFor="addItem">Add Item</label>
-        <input id="addItem"
-            autoFocus
-            ref={inputRef}
-            type='text'
-            placeholder='add item'
-            required
-            value={newItem}
-            onChange={(e) => setNewItem(e.target.value)}
-        />
-        <button type='submit' aria-label='Add Item'> <FaPlus/></button>      
+      <label htmlFor="addItem">Add Item</label>
+      <input id="addItem"
+        autoFocus
+        ref={inputRef}
+        type='text'
+        placeholder='add item'
+        required
+        value={newItem}
+        onChange={(e) => setNewItem(e.target.value)}
+      />
+      <button type='submit' aria-label='Add Item'> <FaPlus /></button>
     </form>
   )
 }
