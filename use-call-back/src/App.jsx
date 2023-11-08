04a7e0ef8f97e80a77b4aada43f1c8e8364711f8
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-08 12:06:01 +0100

    update

--- use-call-back/src/App.jsx
+++ use-call-back/src/App.jsx
@@ -3,8 +3,11 @@ import { useState } from "react"
 import React from 'react'
 
 const App = () => {
+  const [userInput, setUserInput] = useState('')
+  const [num1] = useState(4)
+
   return (
-    <div>App</div>
+    
   )
 }
 
