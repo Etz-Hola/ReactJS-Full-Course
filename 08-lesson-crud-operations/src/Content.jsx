b3533079646aff0dd19e55eff62aef421cc0d867
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-08 12:47:49 +0100

    update

--- 08-lesson-crud-operations/src/Content.jsx
+++ 08-lesson-crud-operations/src/Content.jsx
@@ -2,18 +2,18 @@
 import ItemList from './ItemList'
 
 
-const Content = ({items, handleCheck, handleDelete}) => {
+const Content = ({ items, handleCheck, handleDelete }) => {
 
 
   return (
     <>
       {items.length ? (
 
-    
-      <ItemList items={items} handleCheck={handleCheck}  handleDelete={handleDelete}/>
+
+        <ItemList items={items} handleCheck={handleCheck} handleDelete={handleDelete} />
       ) : (
-        <p style={{marginTop: "2rem"}}>Your list is Empty</p>
-      ) }
+        <p style={{ marginTop: "2rem" }}>Your list is Empty</p>
+      )}
 
 
     </>
