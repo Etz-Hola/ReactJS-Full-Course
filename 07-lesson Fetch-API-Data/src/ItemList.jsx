aee54a8e82b4fc94291fd426614bea461aee5108
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-08 12:36:26 +0100

    h

--- 07-lesson Fetch-API-Data/src/ItemList.jsx
+++ 07-lesson Fetch-API-Data/src/ItemList.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import LineItem from './LineItem'
 
-const ItemList = ({items, handleCheck, handleDelete}) => {
+const ItemList = ({ items, handleCheck, handleDelete }) => {
   return (
     <ul>
-        {items.map((item) => (
-            <LineItem item={item} handleCheck={handleCheck} handleDelete={handleDelete}/>
-        ))}
+      {items.map((item) => (
+        <LineItem item={item} handleCheck={handleCheck} handleDelete={handleDelete} />
+      ))}
     </ul>
   )
 }
