096b7a5e7fa780037e150e9358516ad7b53461a2
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-08 12:00:24 +0100

    u

--- 07-lesson Fetch-API-Data/src/Content.jsx
+++ 07-lesson Fetch-API-Data/src/Content.jsx
@@ -1,23 +1,21 @@
 
 import ItemList from './ItemList'
 
-
-const Content = ({items, handleCheck, handleDelete}) => {
+const Content = ({ items, handleCheck, handleDelete }) => {
 
 
   return (
     <>
       {items.length ? (
 
-    
-      <ItemList items={items} handleCheck={handleCheck}  handleDelete={handleDelete}/>
+        <ItemList items={items} handleCheck={handleCheck} handleDelete={handleDelete} />
       ) : (
-        <p style={{marginTop: "2rem"}}>Your list is Empty</p>
-      ) }
+        <p style={{ marginTop: "2rem" }}>Your list is Empty</p>
+      )}
 
 
     </>
-  )
-}
+  );
+};
 
 export default Content
