336bbf05a118d35e39369a58864514267cab6c79
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-08 12:26:33 +0100

    u

--- 01-lesson Introduction-to-ReactJS/src/InputField.jsx
+++ 01-lesson Introduction-to-ReactJS/src/InputField.jsx
@@ -1,11 +1,11 @@
-import {useState} from 'react'
+import { useState } from 'react'
 
 const InputField = () => {
-    const [text, setText] = useState('');
+  const [text, setText] = useState('');
 
-    const handleChange = (e) => {
-        setText(e.target.value);
-    };
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
   return (
     <div className='main'>
       <input type="text" value={text} onChange={handleChange} />
