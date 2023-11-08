c645c12068636e81fe0c3558869745f28fc68199
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-08 11:43:15 +0100

    update

---
+++ using-prev-function/src/Counter.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+
+const Counter = () => {
+    const [count, setCount] = useState(0)
+    const [values, setValues] = useState({ first: "", last: "",})
+
+    const add = () => {
+        setCount(prev => prev + 1)
+        setCount(prev => prev + 1)
+
+    }
+    const subtract = () => {
+        setCount(count - 4)
+    }
+
+    const updateArray = (newVal) => {
+        setValues({...values, last: "Shola"})
+        setValues(prev => ({...prev, last: "Hassan"}))
+    }
+    updateArray()
+    console.log(values);
+
+  return (
+    <section className="Counter">
+        <h1>{count}</h1>
+        <div className="calcBtn">
+            <button onClick={add}>+</button>
+            <button onClick={subtract}>-</button>
+        </div>
+        <div>
+        <button onClick={() => setCount(0)}>Reset</button>
+        </div>
+    </section>
+  )
+}
+
+export default Counter
