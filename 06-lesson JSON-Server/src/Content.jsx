dcc7c3fe4b92867a63729bf1ee4262fc9691de53
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-08 11:54:45 +0100

    up

--- 06-lesson JSON-Server/src/Content.jsx
+++ 06-lesson JSON-Server/src/Content.jsx
@@ -2,19 +2,14 @@
 import ItemList from './ItemList'
 
 
-const Content = ({items, handleCheck, handleDelete}) => {
-
-
+const Content = ({ items, handleCheck, handleDelete }) => {
   return (
     <main>
       {items.length ? (
-
-    
-      <ItemList items={items} handleCheck={handleCheck}  handleDelete={handleDelete}/>
+        <ItemList items={items} handleCheck={handleCheck} handleDelete={handleDelete} />
       ) : (
-        <p style={{marginTop: "2rem"}}>Your list is Empty</p>
-      ) }
-
+        <p style={{ marginTop: "2rem" }}>Your list is Empty</p>
+      )}
 
     </main>
   )
