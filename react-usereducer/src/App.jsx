1f912e32467bfc1f481f17064d854eaa82ffd9cc
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-09 15:55:32 +0100

    update

--- react-usereducer/src/App.jsx
+++ react-usereducer/src/App.jsx
@@ -1,16 +1,16 @@
 import { useReducer } from "react"
 
 const reducer = (state, action) => {
-  switch(action.type){
+  switch (action.type) {
     case 'increment':
-      return {...state, count: state.count +1}
+      return { ...state, count: state.count + 1 }
     case 'decrement':
-      return {...state, count: state.count -1}
+      return { ...state, count: state.count - 1 }
     case 'newUserInput':
-      return {...state, userInput: action.payload}
+      return { ...state, userInput: action.payload }
     case 'tgColor':
-      return {...state, color: !state.color}
-    default: 
+      return { ...state, color: !state.color }
+    default:
       throw new Error()
   }
 }
@@ -23,33 +23,33 @@ const ACTION = {
 }
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, {count: 0, userInput:'', color: false});
+  const [state, dispatch] = useReducer(reducer, { count: 0, userInput: '', color: false });
   // const [userInput, setUserInput] = useState('');
   // const [count, setCount] = useState(0);
   // const [color, setColor] = useState(false);
-  
-   
+
+
   return (
-    <main className="App" style={{color: state.color ? '#fff' : '#fff952'}}>
+    <main className="App" style={{ color: state.color ? '#fff' : '#fff952' }}>
       <input
         type="text"
         value={state.userInput}
-        onChange={(e) => dispatch({type: 'newUserInput', payload: e.target.value})}        
+        onChange={(e) => dispatch({ type: 'newUserInput', payload: e.target.value })}
       />
 
-      <br/>
-      <br/>
+      <br />
+      <br />
       <p>{state.count}</p>
 
 
       <section>
-        <button onClick={(() => dispatch({type: ACTION.DECREMENT}))}>-</button>
-        <button onClick={(() => dispatch({type: ACTION.INCREMENT}))}>+</button>
-        <button onClick={(() => dispatch({type: ACTION.TG_COLOR}))}>Color</button>
+        <button onClick={(() => dispatch({ type: ACTION.DECREMENT }))}>-</button>
+        <button onClick={(() => dispatch({ type: ACTION.INCREMENT }))}>+</button>
+        <button onClick={(() => dispatch({ type: ACTION.TG_COLOR }))}>Color</button>
       </section>
 
-      <br/>
-      <br/>
+      <br />
+      <br />
 
       <p>{state.userInput}</p>
     </main>
