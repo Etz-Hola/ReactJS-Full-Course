6d562fea972d2f434911ce3296eecc373063c23b
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-09 10:54:51 +0100

    update

--- react-use-memo/src/App.jsx
+++ react-use-memo/src/App.jsx
@@ -6,9 +6,7 @@ const getArray = () => {
   return ['Muhammad', "Fine Boy"]
 }
 
-const fibNumber = useMemo(() => fib(userInput), [userInput, fib])
 
-const myArray = useMemo(() => getArray(), [])
 
 const App = () => {
   const [userInput, setUserInput] = useState('')
@@ -18,14 +16,33 @@ const App = () => {
     return n <= 1 ? n : fib(n - 1) + fib(n - 2)
   }, [])
 
+  const fibNumber = useMemo(() => fib(userInput), [userInput, fib])
+
+const myArray = useMemo(() => getArray(), [])
+
+useEffect(() => {
+  console.log("New Array")
+}, [myArray])
+
 
 
   return (
     <main className="App">
-      <input className="inputText" type="text" placeholder="input"
-        value={userInput} onChange={(e) => setUserInput(e.target.value)}
+      <label>Fibonacci Sequence</label>
+      <input type="text" 
+        placeholder="Position"
+        value={userInput} 
+        onChange={(e) => setUserInput(e.target.value)}
       />
-      <h1>OutPut: <span style={{ color: "aqua" }}> {userInput || ".."}</span></h1>
+
+      <p>Number: {fibNumber || "--"} </p>
+
+      <br/>
+      <br/>
+
+      <label>Random Inpuy:</label>
+      
+      
     </main>
   )
 }
