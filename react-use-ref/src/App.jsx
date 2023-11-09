a3f80593a04d77e53d76566c2dc6cc7d29558f22
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-09 11:57:49 +0100

    u

--- react-use-ref/src/App.jsx
+++ react-use-ref/src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
         type="text"
         value={randomInput}
         placeholder="Type anything..."
-        onChange={(e) => setRandomInput(e.target.value)}
+        
       />
       <p>Result: {randomInput}</p>
 
