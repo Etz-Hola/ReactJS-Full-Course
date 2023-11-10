3afa95ee40a19453bd590aa89843759c19241f79
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-10 10:01:30 +0100

    update

--- react-form-validation/src/Register.jsx
+++ react-form-validation/src/Register.jsx
@@ -1,1 +1,6 @@
 import { useRef, useState, useEffect } from "react";
+import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "./api/axios"
+
+
