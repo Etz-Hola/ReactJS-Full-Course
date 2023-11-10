355929284a8f48b883b983ae5562796ef1cbe890
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-10 10:12:47 +0100

    update

--- react-form-validation/src/Register.jsx
+++ react-form-validation/src/Register.jsx
@@ -4,3 +4,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "./api/axios"
 
 
+const USE_REGEX = /^[A-Z][A-Z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const REGISTER_URL = "./Register.js";
+
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>Register</div>
+  )
+}
+
+export default Register
