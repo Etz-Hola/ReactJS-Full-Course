3d16c6b3d311cfb7331edb69b22546c1bcb6d9d7
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-13 14:34:11 +0100

    up

--- react-form-authentication-login/src/Login.jsx
+++ react-form-authentication-login/src/Login.jsx
@@ -1,1 +1,32 @@
 import { useRef, useState, useEffect } from "react";
+
+const Login = () => {
+    const userRef = useRef()
+    const errRef = useRef()
+
+    const [username, setUserName] = useState("")
+    const [password, setPassword] = useState("")
+    const [errMsg, setErrMsg] = useState("")
+    const [success, setSuccess] = useState(false)
+
+    useEffect(() => {
+        userRef.current.focus()
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('')
+    }, [username, password])
+
+
+  return (
+    <>
+    <section>
+        <p>
+            j
+        </p>
+    </section>
+    </>
+  )
+}
+
+export default Login
