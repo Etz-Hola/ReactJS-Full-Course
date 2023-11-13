0caef75d8dc4fee7495f9e594b47991e50d9f4db
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-13 12:23:05 +0100

    Success

--- react-form-validation/src/Register.jsx
+++ react-form-validation/src/Register.jsx
@@ -8,7 +8,6 @@ const USER_REGEX = /^[A-z][a-z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 const REGISTER_URL = "/register";
 
-
 const Register = () => {
   const userRef = useRef();
   const errRef = useRef();
