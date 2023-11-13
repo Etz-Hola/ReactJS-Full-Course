bfeebc869e7fd55dd25f2c7acf20aa0678c96b30
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-13 11:36:37 +0100

    Registered

--- react-form-validation/src/Register.jsx
+++ react-form-validation/src/Register.jsx
@@ -52,7 +52,7 @@ const Register = () => {
       <section>
           <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive"> {errMsg} </p>
           <h1>Register.</h1>
-          <form>
+          <form onSubmit={handleSubmit}>
             <label htmlFor="username">
               username:
               <FontAwesomeIcon icon={faCheck} className={validName ? "valid" : "hide"} />
