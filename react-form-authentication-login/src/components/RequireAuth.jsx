2bdc20ae3176e31d1f3cfdde64be99ba1ecf95d3
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-14 15:53:04 +0100

    <div>RequireAuth</div>

--- react-form-authentication-login/src/components/RequireAuth.jsx
+++ react-form-authentication-login/src/components/RequireAuth.jsx
@@ -1,7 +1,12 @@
+import { useLocation, Navigate, Outlet } from "react-router-dom"
+import useAuth from "../hooks/useAuth"
+
 
 const RequireAuth = () => {
+    const { auth } = useAuth()
+    const location = useLocation()
   return (
-    <div>RequireAuth</div>
+    
   )
 }
 
