8f9c7eda4125c8fa87e0e25dafcdc46924641064
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-14 16:16:07 +0100

    <Route element={<RequireAuth all

--- react-form-authentication-login/src/App.jsx
+++ react-form-authentication-login/src/App.jsx
@@ -9,6 +9,7 @@ import Editor from './components/Editor'
 import Lounge from './components/Lounge'
 import Admin from './components/Admin'
 import Missing from './components/Missing'
+import RequireAuth from './components/RequireAuth'
 
 const ROLES = {
   'User': 2001,
@@ -24,14 +25,26 @@ const App = () => {
 
         {/* public routes/ */}
         <Route index element={<Login />}/>
-        <Route path='/linkpage' element={<LinkPage />}/>
+          <Route path='/linkpage' element={<LinkPage />}/>
         <Route path='/unauthorized' element={<Unauthorized />}/>
 
         {/* private routes/ */}
-        <Route path='/' element={<Home/>}/>
-        <Route path='editor' element={<Editor/>}/>
-        <Route path='admin' element={<Admin/>}/>
-        <Route path='lounge' element={<Lounge/>}/>
+        <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
+          <Route path='/' element={<Home />} />
+        </Route>
+
+        <Route element={<RequireAuth allowedRoles={[ROLES.Editor]} />}>
+          <Route path='editor' element={<Editor />} />
+        </Route>
+
+        <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
+          <Route path='admin' element={<Admin />} />
+        </Route>
+
+        <Route element={<RequireAuth allowedRoles={[ROLES.Editor, ROLES.Admin]} />}>
+          <Route path='lounge' element={<Lounge />} />
+        </Route>
+
 
         {/* catch all/ */}
         <Route path='*' element={<Missing/>}/>
