d206e33c648a89a6d83261783b0a4b8cb39dc313
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-14 14:55:41 +0100

    <Route path='/' element={<Layout />}>

--- react-form-authentication-login/src/App.jsx
+++ react-form-authentication-login/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout'
+// import Layout from './components/Layout'
 import Login from './components/Login'
 import LinkPage from './components/LinkPage'
 import Unauthorized from './components/Unauthorized'
@@ -9,14 +9,11 @@ const App = () => {
   return (
     <main className='App'>   
       <Routes>
-        <Route path='/' element={<Layout />}>
           {/* public routes/ */}
           <Route path='/' element={<Login />}/>
           <Route path='/linkpage' element={<LinkPage />}/>
           <Route path='/unauthorized' element={<Unauthorized />}/>
-          
 
-        </Route>
     </Routes>    
       </main> 
   )
