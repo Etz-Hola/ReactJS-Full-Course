460de4eb6f6c8e562f2cf9843ae5329e14fb7218
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-14 08:54:46 +0100

    AuthContext

--- react-form-authentication-login/src/context/AuthProvider.jsx
+++ react-form-authentication-login/src/context/AuthProvider.jsx
@@ -3,5 +3,13 @@ import { createContext, useContext, useState } from "react";
 const AuthContext = createContext({})
 
 export const AuthProvider =({children}) => {
-    con
+    const [auth, setAuth] = useState({})
+
+    return(
+        <AuthContext.Provider value={{auth, setAuth}}>
+            {children}
+        </AuthContext.Provider>
+    )
 }
+
+export default AuthContext
