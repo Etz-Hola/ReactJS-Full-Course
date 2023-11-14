710db4d3e53fc75eca218aee97aafb5ee7e55f41
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-14 14:16:09 +0100

    div

---
+++ react-form-authentication-login/src/components/LinkPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const LinkPage = () => {
+  return (
+    <section>
+        <h1>Links</h1>
+        <br />
+        <h2>Publicc</h2>
+        <Link to='/login'></Link>
+        <Link to='/login'></Link>
+
+    </section>
+  )
+}
+
+export default LinkPage
