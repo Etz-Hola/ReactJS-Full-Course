9feee6ec89d757749ad5828cb0f771db88ad7b18
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-14 16:22:42 +0100

    import AuthContext from "../context/AuthProvider";

--- react-form-authentication-login/src/components/Login.jsx
+++ react-form-authentication-login/src/components/Login.jsx
@@ -1,5 +1,4 @@
-import { useRef, useState, useEffect, useContext } from "react";
-import AuthContext from "../context/AuthProvider";
+import { useRef, useState, useEffect } from "react";
 import axios from "../api/axios";
 
 const Login = () => {
