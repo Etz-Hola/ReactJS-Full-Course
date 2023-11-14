2018cc92f6118640785deecd85595d346df78000
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-14 12:49:33 +0100

    Editor

--- react-form-authentication-login/src/components/Editor.jsx
+++ react-form-authentication-login/src/components/Editor.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Editor = () => {
   return (
     <section>
-        <h1>Editor's</h1>
+        <h1>Editor's Page</h1>
+        <br />
+        <p>You must have been assigned an Admin Role</p>
+
+        <div className='flexGrow'>
+            <Link to='/'>Home</Link>
+        </div>
     </section>
   )
 }
