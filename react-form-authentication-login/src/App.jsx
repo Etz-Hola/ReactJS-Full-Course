da2b00dd9e30d9225edcab728fcf7198a854a7f2
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-14 14:44:04 +0100

    Unauthorized

--- react-form-authentication-login/src/App.jsx
+++ react-form-authentication-login/src/App.jsx
@@ -2,18 +2,21 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Login from './components/Login'
+import LinkPage from './components/LinkPage'
+import Unauthorized from './components/Unauthorized'
 
 const App = () => {
-  return (
-    
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          {/* public routes */}
-          <Route path='login' element={<Login />}/>
+  return (    
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        {/* public routes */}
+        <Route path='login' element={<Login />}/>
+        <Route path='linkpage' element={<LinkPage />}/>
+        <Route path='unauthorized' element={<Unauthorized />}/>
+        
 
-        </Route>
-      </Routes>
-    
+      </Route>
+    </Routes>    
   )
 }
 
