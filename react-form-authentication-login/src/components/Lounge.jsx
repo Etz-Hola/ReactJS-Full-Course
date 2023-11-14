2771f3d9c3f4b3f22b1d8944d38640e399e40a21
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-14 14:22:44 +0100

    Lounge

--- react-form-authentication-login/src/components/Lounge.jsx
+++ react-form-authentication-login/src/components/Lounge.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Lounge = () => {
   return (
     <section>
         <h1>The Lounge</h1>
-        
+        <br />
+        <p>Admin and the editor can hang out here</p>
+        <div className="flexGroe">
+            <Link to='/'>Home</Link>
+        </div>
+
     </section>
   )
 }
