119017c5cbdd251e1ec546945989c40deabb9e22
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-15 15:29:48 +0100

    useAxiosPrivate

--- react-form-authentication-login/src/hooks/userAxiosPrivate.js
+++ react-form-authentication-login/src/hooks/userAxiosPrivate.js
@@ -4,7 +4,6 @@ import useRefreshToken from "./useRefreshToken"
 import useAuth from "./useAuth"
 
 
-
 const useAxiosPrivate = () => {
     const refresh = useRefreshToken();
     const {auth} = useAuth()
