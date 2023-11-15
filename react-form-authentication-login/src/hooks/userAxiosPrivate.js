2cda6a27765a01f5b374c967f200984710fd6e26
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-15 15:00:37 +0100

    useAuth

--- react-form-authentication-login/src/hooks/userAxiosPrivate.js
+++ react-form-authentication-login/src/hooks/userAxiosPrivate.js
@@ -1,8 +1,20 @@
+import { axiosPrivate } from "../api/axios"
+import { useEffect } from "react"
+import useRefreshToken from "./useRefreshToken"
+import useAuth from "./useAuth"
+
 
 const userAxiosPrivate = () => {
-  return (
-    <div>userAxiosPrivate</div>
-  )
+    const refresh = useRefreshToken();
+    const {auth} = useAuth()
+
+    useEffect(() => {
+        const requestIntercep = axiosPrivate.interceptors.request.use((config) => {
+            config.headers['Authorization'] = `Bearer ${auth?.accessToken}`
+        })
+
+    }, [auth, refresh])
+  return axiosPrivate
 }
 
 export default userAxiosPrivate
