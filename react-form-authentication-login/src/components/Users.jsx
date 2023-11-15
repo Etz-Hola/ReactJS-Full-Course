cd26ce5f6988cfa8f9c1ab7d5d6bd2d77ac82f3e
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-15 15:38:34 +0100

    useState

--- react-form-authentication-login/src/components/Users.jsx
+++ react-form-authentication-login/src/components/Users.jsx
@@ -1,11 +1,10 @@
 import { useState, useEffect } from "react";
-import axios from "../api/axios";
-// import useRefreshToken from "../hooks/useRefreshToken";
+import useAxiosPrivate from "../hooks/userAxiosPrivate";
 
 
 const Users = () => {
     const [users, setUsers] = useState()
-    const refresh = useRefreshToken()
+    const axiosPrivate = useAxiosPrivate()
 
     useEffect(() => {
         let isMounted = true;
@@ -14,7 +13,7 @@ const Users = () => {
         const getUsers = async () => {
 
             try {
-                const response = await axios.get('/users', {
+                const response = await axiosPrivate.get('/users', {
                     signal: controller.signal
                 })
                 console.log(response.data)
