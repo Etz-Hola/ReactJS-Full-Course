877c35f452dac2142121f2d19bdd01d7193b295a
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-16 12:22:09 +0100

    </Route>

--- react-form-authentication-login/src/components/PersistLogin.jsx
+++ react-form-authentication-login/src/components/PersistLogin.jsx
@@ -35,7 +35,7 @@ const PersistLogin = () => {
   return (
     <>
         {isLoading ? 
-            <p>Loading...  .   ....</p>
+            <p>Loading...     ....</p>
             :
             <Outlet />
         }
