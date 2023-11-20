24d6ef21acdbcfe0dd3e8d473e0d72b884e7915f
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-20 10:52:26 +0100

    import useLocalStorage from "../hooks/useLocalStorage";

--- react-form-authentication-login/src/components/Login.jsx
+++ react-form-authentication-login/src/components/Login.jsx
@@ -2,7 +2,6 @@ import { useRef, useState, useEffect } from "react";
 import axios from "../api/axios";
 import useAuth from "../hooks/useAuth";
 import { useNavigate, Link, useLocation } from "react-router-dom";
-import useLocalStorage from "../hooks/useLocalStorage";
 import useInput from "../hooks/useInput";
 
 const LOGIN_URL = './auth'
