5f041e6a296f382daa44cb0a4fa14b94460a8623
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-20 10:02:36 +0100

    update

--- react-form-authentication-login/src/hooks/useLocalStorage.js
+++ react-form-authentication-login/src/hooks/useLocalStorage.js
@@ -1,13 +1,22 @@
 import { useState, useEffect } from "react"
 
+const getLocalValue = (key, initValue) => {
+    // SSR Next Js
+    if (typeof window === 'undefined') return initValue
+
+    // if a value is already stored
+
+    const
+}
+
 const useLocalStorage = (key, initValue) => {
     const [value, setValue] = useState(JSON.parse(localStorage.getItem(key)) || initValue)
 
     useEffect(() => {
         localStorage.setItem(key, JSON.stringify(value))
     }, [key, value])
-    
+
   return [value, setValue]
 }
 
-export default useLocalStorage
+export default useLocalStorage 
