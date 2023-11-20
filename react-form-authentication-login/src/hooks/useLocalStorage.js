7adfc58762584ba9560fca5addb934b9559ae884
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-20 10:17:59 +0100

    JSON.parse(localStorage.getItem(key)) || initValue

--- react-form-authentication-login/src/hooks/useLocalStorage.js
+++ react-form-authentication-login/src/hooks/useLocalStorage.js
@@ -17,7 +17,9 @@ const getLocalValue = (key, initValue) => {
 }
 
 const useLocalStorage = (key, initValue) => {
-    const [value, setValue] = useState(JSON.parse(localStorage.getItem(key)) || initValue)
+    const [value, setValue] = useState(() => {
+        JSON.parse(localStorage.getItem(key)) || initValue
+    })
 
     useEffect(() => {
         localStorage.setItem(key, JSON.stringify(value))
