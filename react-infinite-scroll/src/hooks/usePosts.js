9d91151e04d583a22e06aa0cd5a41331d690321e
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-22 10:22:42 +0100

    import React from 'react'

---
+++ react-infinite-scroll/src/hooks/usePosts.js
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+import { getPostsPage } from "../api/axios";
+
+
+const usePosts = () => {
+  return (
+    <div>usePosts</div>
+  )
+}
+
+export default usePosts
