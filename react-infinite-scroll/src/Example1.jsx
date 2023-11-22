b4b984dfca6fd6d8c46de1cb1a2fe280959e0194
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-22 10:01:51 +0100

    d

--- react-infinite-scroll/src/Example1.jsx
+++ react-infinite-scroll/src/Example1.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+
 
 const Example1 = () => {
   return (
     <>
         <h1 id='top'>
-            
+            &infin; Infinite Query &amp; Scroll
+            <br />
+            &infin; Ex. 1 - React Only
         </h1>
+
+        <p className='center'>
+            <a href="#top">Back to Top</a>
+        </p>
     
     </>
   )
