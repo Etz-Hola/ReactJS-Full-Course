88062c471bfee6a5008d55542d2b35770f461d5d
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-23 15:08:25 +0100

    b

--- threads-social-app/src/components/UserHeader.jsx
+++ threads-social-app/src/components/UserHeader.jsx
@@ -1,12 +1,31 @@
 import { Box, Flex, VStack, Text } from "@chakra-ui/layout"
+import { Avatar } from "@chakra-ui/react"
 
 const UserHeader = () => {
   return ( <VStack gap={4} alignItems={'start'}>
     <Flex justifyContent={'space-between'} w={'full'}>
         <Box>
-            <Text fontSize={"2xl"} fontWeight={"bold"} >
+            <Text 
+                fontSize={"2xl"} 
+                fontWeight={"bold"} >
                 Aliu Musa
             </Text>
+            <Flex  gap={2} alignItems={"center"}>
+                <Text fontSize={"sm"}>aliumusa@99</Text>
+                <Text fontSize={'xs'}
+                bg={"gray.dark"}
+                color={"gray.light"}
+                p={1}
+                borderRadius={"full"}>threads.net</Text>
+            </Flex>
+        </Box>
+
+        <Box>
+            <Avatar 
+                name="Aliu Musa"
+                src="/directorPro.jpeg"
+                size={"x"}
+            />
         </Box>
 
     </Flex>
