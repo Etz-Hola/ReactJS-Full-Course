b91bd1ed6c38429c2155fa76dab523fc9073b0bb
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-23 14:44:03 +0100

    b

--- threads-social-app/src/components/UserHeader.jsx
+++ threads-social-app/src/components/UserHeader.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { Box, Flex, VStack, Text } from "@chakra-ui/layout"
 
 const UserHeader = () => {
-  return (
-    <div>UserHeader</div>
+  return ( <VStack gap={4} alignItems={'start'}>
+    <Flex justifyContent={'space-between'} w={'full'}>
+        <Box>
+            <Text >
+                Aliu Musa
+            </Text>
+        </Box>
+
+    </Flex>
+  </VStack>
   )
 }
 
