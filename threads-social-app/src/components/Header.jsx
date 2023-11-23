cff0874e344ca0cec9feb04e021b8af715bb0483
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-23 12:56:05 +0100

    ui

--- threads-social-app/src/components/Header.jsx
+++ threads-social-app/src/components/Header.jsx
@@ -1,7 +1,12 @@
-import { Flex } from "@chakra-ui/react"
+import { Flex, Image } from "@chakra-ui/react"
 
 const Header = () => {
-  return <Flex justifyContent={"center"} mt={6} mb="12"></Flex>
+  return <Flex justifyContent={"center"} mt={6} mb="12">
+    <Image alt="logo"
+        w={6}
+        src={colorMode === dark ? "/light-logo.svg" : "/dark-logo.svg"}/>
+    />
+  </Flex>
 }
 
 export default Header
