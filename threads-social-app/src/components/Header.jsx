e13e598b37fd95d686e3c8da5adcd4370aa8bf40
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-23 12:50:01 +0100

    z

--- threads-social-app/src/components/Header.jsx
+++ threads-social-app/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { Flex } from "@chakra-ui/react"
 
 const Header = () => {
-  return <Flex justifyContent={"center"} mt={6}></Flex>
+  return <Flex justifyContent={"center"} mt={6} mb={}></Flex>
 }
 
 export default Header
