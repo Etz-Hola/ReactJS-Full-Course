1637ed726c28c69e97d82a5bced1a60a01ca10c9
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-23 11:27:54 +0100

    ,

--- threads-social-app/src/main.jsx
+++ threads-social-app/src/main.jsx
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import {mode} from "@chakra-ui/theme-tools"
+import { extendTheme } from '@chakra-ui/theme-utils'
+
+const styles = {
+  global: (props) => ({
+    body: {
+      color: mode("gray.800", "whiteAilpha:900")(props),
+      bg: mode("gray.100", "#101010")(props),
+    },
+  }),
+};
+
+const config = {
+  initialColorMode: 'light',
+  useSystemColorMode: false,
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
