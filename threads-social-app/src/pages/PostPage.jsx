4265f7edb62c55d5672132c7076b7d912f1d8062
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-27 15:05:30 +0100

    mkkkk

--- threads-social-app/src/pages/PostPage.jsx
+++ threads-social-app/src/pages/PostPage.jsx
@@ -1,28 +1,27 @@
 import { Avatar, Flex, Image, Text } from '@chakra-ui/react'
 import React from 'react'
-import { BsThreeDots} from "react-icons/bs"
+import { BsThreeDots } from "react-icons/bs"
 
 const PostPage = () => {
   return (
     <>
-      <Flex>
-        <Flex w={"full"} gap={3} alignItems={"center"} justifyContent={"center"}>
-          <Avatar src="/aliumusa.jpeg" name='Aliu Musa' size={"md"} />
+      <Flex w={"full"} gap={3} alignItems={"center"} justifyContent={"space-between"}>
 
-          <Flex alignItems={"center"}>
+        <Flex alignItems={"center"}>
+        <Avatar src="/aliumusa.jpeg" name='Aliu Musa' size={"md"} mr={2}/>
           <Text fontSize={"sm"}>Aliu Musa</Text>
-          <Image src='/verified.png' h={4} w={4} ml={2}/>
-          </Flex>
-
-
-          <Flex gap={4} alignItems={"center"}>
-          <Text fontSize={{base: "xs", md: "sm"}} textAlign={"right"} w={36} color={"gray.light"}>2dat</Text>
-          <BsThreeDots />
-          </Flex>
-
-
+          <Image src='/verified.png' h={4} w={4} ml={2} />
         </Flex>
+
+
+        <Flex gap={4} alignItems={"center"}>
+          <Text fontSize={{ base: "xs", md: "sm" }} textAlign={"right"} w={36} color={"gray.light"}>2dat</Text>
+          <BsThreeDots />
+        </Flex>
+
+
       </Flex>
+
 
     </>
   )
