21a2756da9a8c975fb18da4717a9c97b7bb2c698
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-27 16:17:05 +0100

    dd

--- threads-social-app/src/components/Comments.jsx
+++ threads-social-app/src/components/Comments.jsx
@@ -1,8 +1,10 @@
 import { Avatar, Flex, Text } from "@chakra-ui/react"
-import React from "react"
+import React, { useState } from "react"
 import { BsThreeDots } from "react-icons/bs"
+import Actions from "./Actions"
 
 const Comments = ({comment, createdAt, likes, username, userAvater}) => {
+    const [liked, setLiked] = useState(false)
   return (
     <React.Fragment>
         <Flex gap={4} my={2} w={"full"}>
@@ -12,7 +14,7 @@ const Comments = ({comment, createdAt, likes, username, userAvater}) => {
                 name= "Ryan Florence"             
             />
 
-            <Flex flexDirection={"column"} gap={1} w={"full"} border={"2px solid white"}>
+            <Flex flexDirection={"column"} gap={1} w={"full"}>
 
                 <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"}>
                     <Text fontSize={"sm"} fontWeight={"bold"} >JohnDoe</Text>
@@ -22,6 +24,11 @@ const Comments = ({comment, createdAt, likes, username, userAvater}) => {
                         <BsThreeDots />
                     </Flex>
                 </Flex>
+                <Text>{comment}</Text>
+
+                <Actions liked={liked} setLiked={setLiked}/>
+
+                <Text>{12 + (liked ? 1 : 0)}likes</Text>
 
             </Flex>
         </Flex>
