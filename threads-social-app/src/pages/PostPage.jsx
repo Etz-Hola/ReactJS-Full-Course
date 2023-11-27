574d45abaa18aec278a8c5ae77758015121f4c0d
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-27 15:19:12 +0100

    k

--- threads-social-app/src/pages/PostPage.jsx
+++ threads-social-app/src/pages/PostPage.jsx
@@ -1,30 +1,59 @@
-import { Avatar, Flex, Image, Text } from '@chakra-ui/react'
-import React from 'react'
-import { BsThreeDots } from "react-icons/bs"
+import { Avatar, Box, Flex, Image, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { BsThreeDots } from "react-icons/bs";
+import Actions from "../components/Actions";
 
 const PostPage = () => {
+  const [ liked, setLiked] = useState(false)
   return (
     <>
-      <Flex w={"full"} gap={3} alignItems={"center"} justifyContent={"space-between"}>
-
+      <Flex
+        w={"full"}
+        gap={3}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+      >
         <Flex alignItems={"center"}>
-        <Avatar src="/aliumusa.jpeg" name='Aliu Musa' size={"md"} mr={2}/>
+          <Avatar src="/aliumusa.jpeg" name="Aliu Musa" size={"md"} mr={2} />
           <Text fontSize={"sm"}>Aliu Musa</Text>
-          <Image src='/verified.png' h={4} w={4} ml={2} />
+          <Image src="/verified.png" h={4} w={4} ml={2} />
         </Flex>
 
-
         <Flex gap={4} alignItems={"center"}>
-          <Text fontSize={{ base: "xs", md: "sm" }} textAlign={"right"} w={36} color={"gray.light"}>2dat</Text>
+          <Text
+            fontSize={{ base: "xs", md: "sm" }}
+            textAlign={"right"}
+            w={36}
+            color={"gray.light"}
+          >
+            2dat
+          </Text>
           <BsThreeDots />
         </Flex>
       </Flex>
 
       <Text my={3}>Hello gbogbo ile!!!</Text>
 
+     
+      <Box
+        overflow={"hidden"}
+        borderRadius={"6"}
+        border={"1px solid"}
+        borderColor={"gray.light"}>
+        <Image src="/post1.png" width={"full"} />
+      </Box>
 
+      <Flex>
+        <Actions liked={liked} setLiked={setLiked} />
+      </Flex>  
+
+      <Flex gap={2} color={"gray.light"} fontSize={"sm"} alignItems={"center"}>
+        <Text>200 replies</Text>
+        <Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"}></Box>
+        <Text>21 likes</Text>
+      </Flex>
     </>
-  )
-}
+  );
+};
 
-export default PostPage
+export default PostPage;
