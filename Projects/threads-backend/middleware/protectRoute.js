6d594df934d67ee55fc3fa53cce199f1d43ee77c
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-28 12:44:53 +0100

    zz

---
+++ Projects/threads-backend/middleware/protectRoute.js
@@ -0,0 +1,17 @@
+import { Jwt } from "jsonwebtoken";
+
+
+const protectRoute = async (req, res, next) => {
+    try {
+        const token = req.cookies.jwt;
+
+        if(!token) return res.status(401).json({message: "Unuthorized"})
+        
+    } catch (error) {
+        res.status(500).json({message: error.message});
+        console.log('Error while signing up user: ', error.message)        
+    }
+};
+
+
+export default protectRoute
