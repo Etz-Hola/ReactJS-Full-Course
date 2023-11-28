15c11ab73dbd907d848274afc577d849bd1c197a
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-28 15:08:44 +0100

    nn

--- Projects/threads-backend/controllers/userController.js
+++ Projects/threads-backend/controllers/userController.js
@@ -1,6 +1,17 @@
-
+const User = require('../models/userModel')
+const bcrybt = require('bcryptjs')
 
 const signUpUser = async (req, res) => {
+    try {
+        const {name, email, username, password} = req.body;
+        const user = await user.findOne({$or:[{email}, {username}]})
+
+        if (user) {
+            return res.status(400).json({message: 'User already exists'})
+        }
+    } catch (error) {
+        
+    }
   
 };
 
