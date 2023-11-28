93328f73296ea0079f61f69bb1aa97b24e820f77
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-28 12:25:37 +0100

    mm

--- Projects/threads-backend/models/postModel.js
+++ Projects/threads-backend/models/postModel.js
@@ -3,8 +3,32 @@ import mongoose from "mongoose";
 const postSchema = mongoose.Schema(
     {
         postedBy : {
-            type: 
-        }
+            type: mongoose.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        text: {
+            type: String,
+            maxLength: 500
+        },
+        img: {
+            type: String,String
+        },
+        likes: {
+            //array containing the user ids
+            type: [mongoose.Schema.Types.ObjectId],
+            ref: "User",
+            require: true
+        },
+        replies: [
+            {
+                userId: {
+                    type: [mongoose.Schema.Types.ObjectId],
+                    ref: "User",
+                    require: true
+                }
+            }
+        ]
     }
 )
 
