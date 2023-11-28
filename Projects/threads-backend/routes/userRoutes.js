a8a63ef730b720d412881fcb247a729175337af1
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-28 13:09:39 +0100

    ss

---
+++ Projects/threads-backend/routes/userRoutes.js
@@ -0,0 +1,9 @@
+import { express } from "express";  
+import { signUpUser } from "../controllers/userController";
+
+const router = express.Router();
+
+router.post('/signup', signUpUser)
+
+
+export default router;
--- Projects/threads-backend/server.js
+++ Projects/threads-backend/server.js
@@ -2,10 +2,9 @@ import express from 'express'
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import mongoose from 'mongoose';
+import userRoutes from './routes/userRoutes';
 
 dotenv.config();
-
-
 
 const app = express()
 
@@ -14,6 +13,8 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json({limit : "50mb"})); //parse json data inside the req body
 app.use(express.urlencoded({extended: true})); //parse form data inside the req body
 app.use(cookieParser())
+
+app.use("/api/users", userRoutes)
 
 app.get("/", (req, res) => {
     res.send("Welcome Home 🏠")
