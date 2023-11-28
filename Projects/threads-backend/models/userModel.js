e48c3096d4c6769e202b7934e8e8a05032ace2e6
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-28 12:07:41 +0100

    nn

---
+++ Projects/threads-backend/models/userModel.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+    {
+        name: {
+            type: 'string',
+            required: true
+        },
+        username: {
+            type: 'string',
+            required: true,
+            unique: true
+        },
+        email: {
+            type: 'string',
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: 'string',
+            minLenght: 6,
+            required: true
+        },
+        profilePic: {
+            type: "String",
+            default: "",
+        },
+        followers: {
+            type: [String],
+            default: []
+        },
+        bio: {
+            type: 'String',
+            default: "",
+        },
+        isFrozen: {
+            type: 'Boolean',
+            default: false
+        },
+    },
+    {timestamps: true}
+)
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
