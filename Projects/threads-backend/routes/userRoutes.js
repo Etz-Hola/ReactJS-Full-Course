c62665083f0da1c49e347f422dbfb6b3b95f1328
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-29 15:05:00 +0100

    s

--- Projects/threads-backend/routes/userRoutes.js
+++ Projects/threads-backend/routes/userRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const {
-    getUserProfile, followUnfollowUser, signUpUser, loginUser, logoutUser,
-} = require("../controllers/userController");
+    getUserProfile, followUnfollowUser, signUpUser, loginUser, logoutUser,} = require("../controllers/userController");
 const protectRoute = require("../middleware/protectRoute");
 
 const router = express.Router();
