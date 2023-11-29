a65cdc47183f9c29e10d826b93f48dec32d2ed32
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-29 15:07:28 +0100

    h

--- Projects/threads-backend/routes/userRoutes.js
+++ Projects/threads-backend/routes/userRoutes.js
@@ -5,7 +5,7 @@ const protectRoute = require("../middleware/protectRoute");
 const router = express.Router();
 
 router.get("/profile/:query", getUserProfile);
-// router.l
+// router.
 router.post("/signup", signUpUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
