9d96aa2d30c9dba5bd78a260822716200d8a4f37
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-30 09:25:58 +0100

    x

--- Projects/threads-backend/controllers/userController.js
+++ Projects/threads-backend/controllers/userController.js
@@ -153,7 +153,7 @@ const updateUser = async (req, res) => {
     const userId = req.user._id
     try {
         let user = await User.findById(userId)
-        if (!user) return res.status(400).json({message: "User"})
+        if (!user) return res.status(400).json({message: "User not found"})
         
     } catch (error) {
         res.status(500).json({message: err.message}); //internal server error
