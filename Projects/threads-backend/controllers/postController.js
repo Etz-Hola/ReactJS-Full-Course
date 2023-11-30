6bdeec5dcc10ed6eb104bcef894bfad18a1834a1
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-30 15:50:13 +0100

    hh

--- Projects/threads-backend/controllers/postController.js
+++ Projects/threads-backend/controllers/postController.js
@@ -137,9 +137,7 @@ const getFeedPost = async (req, res) => {
         const following = req.user.following;
         const posts = await Post.find({postedBy: {$in: following}}).sort({createdAt: -1})
 
-        res.status(200).json({posts})        
-        
-    
+        res.status(200).json({posts})
         
     } catch (error) {
         res.status(500).json({ message: error.message }); //Internal server error
