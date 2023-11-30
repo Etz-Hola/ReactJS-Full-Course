e2353cebc079ee18d8fdeef6d65dc1b0101ac92b
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-30 12:08:22 +0100

    nn

--- Projects/threads-backend/controllers/postController.js
+++ Projects/threads-backend/controllers/postController.js
@@ -42,7 +42,6 @@ const getPost = async (req, res) => {
         }
 
         res.status(200).json({post})
-
     } catch (err) {
         res.status(500).json({ message: err.message }); //internal server error
         console.log("Error in Get Post: ", err.message);
