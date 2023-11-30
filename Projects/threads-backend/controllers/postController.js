779941334b0fb8cb6aa4412cc47c296126cde876
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-30 10:58:49 +0100

    ss

--- Projects/threads-backend/controllers/postController.js
+++ Projects/threads-backend/controllers/postController.js
@@ -12,7 +12,7 @@ const createPost = async (req, res) => {
         const user = await User.findById(postedBy);
     } catch (err) {
         res.status(500).json({ message: err.message }); //internal server error
-        console.log("Error in Create Post: ", err.message);      
+        console.log("Error in Create Post: ", err.message);     
     }
 
 }
