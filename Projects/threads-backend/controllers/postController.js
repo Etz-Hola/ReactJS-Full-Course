a420c873e7acf8a6332b3a8b5c23437b539762d1
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-30 12:05:31 +0100

    h

--- Projects/threads-backend/controllers/postController.js
+++ Projects/threads-backend/controllers/postController.js
@@ -38,7 +38,7 @@ const getPost = async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
         if(!post){
-            return res.status(404).json({message: "Post not found"}) 
+            return res.status(404).json({message: "Post not found"})  
         }
         
         
