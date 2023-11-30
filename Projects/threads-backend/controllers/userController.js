ec3133aabf3cd926a27ebffed9cca525f7b7e90d
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-30 09:30:36 +0100

    s

--- Projects/threads-backend/controllers/userController.js
+++ Projects/threads-backend/controllers/userController.js
@@ -154,6 +154,14 @@ const updateUser = async (req, res) => {
     try {
         let user = await User.findById(userId)
         if (!user) return res.status(400).json({message: "User not found"})
+
+        if (password){
+            const salt = await bcrypt.genSalt(10)
+            const hashedPassword = await bcrypt.hash(password, salt)
+            user.password = hashedPassword
+        }
+
+        user
         
     } catch (error) {
         res.status(500).json({message: err.message}); //internal server error
