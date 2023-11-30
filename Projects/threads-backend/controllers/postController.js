6904f09a55d838183aab4660f618c5eba021c497
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-30 10:55:46 +0100

    dd

--- Projects/threads-backend/controllers/postController.js
+++ Projects/threads-backend/controllers/postController.js
@@ -1,4 +1,18 @@
+const User = require("../models/userModel")
+
+
 const createPost = async (req, res) => {
+    try {
+        const {postedBy, text, img} = req.body;
+
+        if(!postedBy || !text) {
+            return res.status(404).json({message: "postedBy and text fields are required"})
+        }
+
+        const user = await User.findById(postedBy);
+    } catch (error) {
+        
+    }
 
 }
 
