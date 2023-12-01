7ade4cecec9a47bc5740536ee1aae1e0df63229c
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-12-01 12:36:19 +0100

    a

--- Projects/threads-backend/controllers/userController.js
+++ Projects/threads-backend/controllers/userController.js
@@ -146,7 +146,7 @@ const followUnfollowUser = async (req, res) => {
 };
 
 // const updateOneUser = async (req, res) => {
-//   const { name, email, username, password, profilePic, bio } = req
+//   const { name, email, username, password, profilePic, bio } = re
   
 
 
