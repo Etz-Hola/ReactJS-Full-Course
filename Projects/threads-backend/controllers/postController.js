dd92f203d2173d44ccdfb419a2ffd4fc5bc0a607
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-12-04 09:18:21 +0100

    xcc

--- Projects/threads-backend/controllers/postController.js
+++ Projects/threads-backend/controllers/postController.js
@@ -153,6 +153,20 @@ const getFeedPost = async (req, res) => {
     }
 }
 
+const getUserPost = async (req, res) => {
+    const {username} = req.params;
+    try {
+        const user = await User.findOne({username})
+        
+        if(!user){
+            return res.status(404).json({message: "User not found"})
+        }
+
+    } catch (error) {
+        
+    }
+}
+
 module.exports = {
     createPost,
     getPost,
