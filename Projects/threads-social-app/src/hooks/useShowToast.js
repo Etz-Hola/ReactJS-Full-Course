76f1547b625294a4f448a71b9ace7823c1116ca2
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-12-04 12:55:50 +0100

    bb

--- Projects/threads-social-app/src/hooks/useShowToast.js
+++ Projects/threads-social-app/src/hooks/useShowToast.js
@@ -2,15 +2,14 @@ import { useToast } from "@chakra-ui/react"
 import { useCallback } from "react"
 
 const useShowToast = () => {
-    const toast = useToast();
-    
+    const toast = useToast();    
     const showToast = useCallback(
         (title, description, status) => {
             toast({
                 title,
                 description,
                 status,
-                duration: 5000,
+                duration: 3000,
                 isClosable: true,
             })        
         },
