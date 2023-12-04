5604e6939b4c3c47bc689d9ce8c5d49692c9188d
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-12-04 10:35:32 +0100

    xx

--- Projects/threads-social-app/src/components/SignUpCard.jsx
+++ Projects/threads-social-app/src/components/SignUpCard.jsx
@@ -23,9 +23,7 @@ export default function SignupCard() {
   return (
     <Flex
       align={"center"}
-      justify={"center"}
-      bg={useColorModeValue("gray.50", "gray.800")}
-    >
+      justify={"center"}    >
       <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
         <Stack align={"center"}>
           <Heading fontSize={"4xl"} textAlign={"center"}>
