0158c060669290c38a310d0436ad4346994c0edc
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-12-05 15:22:49 +0100

    nnn

--- Projects/threads-social-app/src/pages/UpdateProfilePage.jsx
+++ Projects/threads-social-app/src/pages/UpdateProfilePage.jsx
@@ -29,7 +29,7 @@ export default function UpdateProfilePage() {
     const fileRef = useRef(null)
     const { handleImageChange, imgUrl} = usePreviewImg() 
 
-    // consol
+    // cons
   return (
     <Flex
       align={'center'}
