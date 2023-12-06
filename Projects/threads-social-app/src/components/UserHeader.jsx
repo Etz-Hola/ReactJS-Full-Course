91128a0837f8984d9c8d3c731cda9255a2b917c9
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-12-06 10:50:57 +0100

    n

--- Projects/threads-social-app/src/components/UserHeader.jsx
+++ Projects/threads-social-app/src/components/UserHeader.jsx
@@ -39,7 +39,7 @@ const UserHeader = ({ user }) => {
                         borderRadius={"full"}>threads.net</Text>
                 </Flex>
             </Box>
-{/* i */}
+{/*  */}
             <Box>
                 {user.profilePic && (
                     <Avatar
