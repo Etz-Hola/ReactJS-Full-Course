a89742863b70e33d441472b542c3b8cc42a0aded
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-12-07 15:37:02 +0100

    zz

--- Projects/threads-social-app/src/pages/UserPage.jsx
+++ Projects/threads-social-app/src/pages/UserPage.jsx
@@ -3,6 +3,7 @@ import UserHeader from "../components/UserHeader"
 import UserPosts from "../components/UserPosts"
 import { useParams } from 'react-router-dom';
 import useShowToast from '../hooks/useShowToast';
+import { Flex } from '@chakra-ui/react';
 
 const UserPage = () => {
   const [user, setUser] = useState(null);
@@ -32,8 +33,12 @@ const UserPage = () => {
     getUser();
   }, [username, showToast])
 
-  if (!user && loading)
-    return null
+  if (!user && loading){
+    return (
+      <Flex justifyContent={"center"} />
+    )
+  }
+    
 
 
   return (
