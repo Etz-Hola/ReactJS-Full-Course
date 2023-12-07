aafcc03b9fc909d5b397c018108fe8db5709f03a
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-12-07 11:40:37 +0100

    gg

--- Projects/threads-social-app/src/components/CreatePosts.jsx
+++ Projects/threads-social-app/src/components/CreatePosts.jsx
@@ -24,6 +24,7 @@ import { useRecoilState, useRecoilValue } from 'recoil'
 import userAtom from '../atoms/userAtom'
 import useShowToast from '../hooks/useShowToast'
 import { useParams } from 'react-router-dom'
+import postsAtom from '../atoms/postsAtom'
 
 
 const MAX_CHAR = 500
@@ -37,7 +38,7 @@ const CreatePosts = () => {
     const [loading, setLoading] = useState(false)
     const user = useRecoilValue(userAtom)
     const showToast = useShowToast()
-    const username = useParams()
+    const {username} = useParams()
     const [posts, setPosts] = useRecoilState(postsAtom)
 
 
@@ -56,7 +57,7 @@ const CreatePosts = () => {
     const handleCreatePost = async () => {
         setLoading(true)
         try {
-            const res = await fatch("/api/posts/create", {
+            const res = await fetch("/api/posts/create", {
                 method: "POST",
                 headers: {
                     "content-type": "application/json",
