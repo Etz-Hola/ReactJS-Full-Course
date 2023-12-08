54508e9c8431ed6bf5cf0d53beb4be1ae8b3290f
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-12-08 10:26:49 +0100

    ryjttdyh

--- Projects/threads-social-app/src/pages/HomePage.jsx
+++ Projects/threads-social-app/src/pages/HomePage.jsx
@@ -1,8 +1,32 @@
 import { Button, Flex } from '@chakra-ui/react'
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import useShowToast from '../hooks/useShowToast'
 
 const HomePage = () => {
+  const showToast = useShowToast()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {    
+    const getFeedPosts = async () => {
+      setLoading(true);
+      
+      try {
+        const res = await fetch("/api/posts/feed")
+      } catch (err) {
+        console.log(err)
+        showToast("Error", err.message, "error");
+      }finally {
+        setLoading(false);
+      }
+    };
+
+
+    getFeedPosts();
+  }, [showToast])
+  
+
+
   return (
     <>
         <Link to={"/markzuckerberg"}>
