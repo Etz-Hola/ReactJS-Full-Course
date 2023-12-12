15c078e8ad5254f13dd0429097a5ceee402c0980
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-12-12 17:23:39 +0100

    update

--- Projects/threads-social-app/src/components/Header.jsx
+++ Projects/threads-social-app/src/components/Header.jsx
@@ -1,39 +1,44 @@
-import { Flex, Image, Link, useColorMode } from "@chakra-ui/react"
-import { useRecoilValue } from "recoil"
-import userAtom from "../atoms/userAtom"
-import { AiFillHome } from "react-icons/ai"
-import { RxAvatar } from "react-icons/rx"
-import { Link as RouterLink } from "react-router-dom"
+import { Flex, Image, Link, useColorMode } from "@chakra-ui/react";
+import { useRecoilValue } from "recoil";
+import userAtom from "../atoms/userAtom";
+import { AiFillHome } from "react-icons/ai";
+import { RxAvatar } from "react-icons/rx";
+import { Link as RouterLink } from "react-router-dom";
 // import RxAvatar from "./RxAvatar"
 
-
 const Header = () => {
-  const { colorMode, toggleColorMode } = useColorMode()
-  const user = useRecoilValue(userAtom)
+  const { colorMode, toggleColorMode } = useColorMode();
+  const user = useRecoilValue(userAtom);
 
+  return (
+    <Flex justifyContent={"space-between"} gap={300} mt={6} mb="12">
+      {user && (
+        <Flex>
+          <Link as={RouterLink} to="/">
+            <AiFillHome size={24} />
+          </Link>
+        </Flex>
+      )}
 
-  return <Flex justifyContent={"space-between"} gap={300} mt={6} mb="12">
-    {user && (
-      <Link as={RouterLink} to="/">
-        <AiFillHome size={24} />
-      </Link>
-    )}
-
-    {/* {!user && (
+      {/* {!user && (
         <Link as={RouterLink} to={"/auth"} onClick={() => setAuthScreen("login")}> Login </Link>
       )} */}
 
-    <Image className="toggle" alt="logo" cursor="pointer"
-      w={6}
-      src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
-      onClick={toggleColorMode}
-    />
-    {user && (
-      <Link as={RouterLink} to={`/${user.username}`}>
-        <RxAvatar size={24} />
-      </Link>
-    )}
-  </Flex>
-}
+      <Image
+        className="toggle"
+        alt="logo"
+        cursor="pointer"
+        w={6}
+        src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
+        onClick={toggleColorMode}
+      />
+      {user && (
+        <Link as={RouterLink} to={`/${user.username}`}>
+          <RxAvatar size={24} />
+        </Link>
+      )}
+    </Flex>
+  );
+};
 
-export default Header
+export default Header;
