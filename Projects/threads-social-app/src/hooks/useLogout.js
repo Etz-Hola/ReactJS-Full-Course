5bef13c01d65052f032db636219073ee8ef62f55
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-12-13 10:07:38 +0100

    hjh

--- Projects/threads-social-app/src/hooks/useLogout.js
+++ Projects/threads-social-app/src/hooks/useLogout.js
@@ -1,34 +1,34 @@
-import { useRecoilState } from 'recoil'
-import userAtom from '../atoms/userAtom'
-import useShowToast from './useShowToast'
+import { useRecoilState } from "recoil";
+import userAtom from "../atoms/userAtom";
+import useShowToast from "./useShowToast";
 
 const useLogout = () => {
-    const setUser = useRecoilState(userAtom)
-    const showToast = useShowToast()
+  const setUser = useRecoilState(userAtom);
+  const showToast = useShowToast();
 
-    const logout = async () => {
-        try {
-            const res = await fetch("/api/users/logout", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                }
-            })
-            const data = await res.json();
+  const logout = async () => {
+    try {
+      const res = await fetch("/api/users/logout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
 
-            if(data.error) {
-                showToast("Error", data.error, "error")
-                return;
-            }
-            localStorage.removeItem("user-threads")
-            setUser(null)
-        } catch (error) {
-            showToast("Error", error, "error")            
-        }
+      if (data.error) {
+        showToast("Error", data.error, "error");
+        return;
+      }
+      localStorage.removeItem("user-threads");
+      setUser(null);
+    } catch (error) {
+      showToast("Error", error, "error");
     }
-  return logout
-}
+  };
+  return logout;
+};
 
-export default useLogout
+export default useLogout;
 
 //////w/////////////
